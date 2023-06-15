f84fb55d17cae9bea5e2d21991fafe21e13bb978
Shigema <tsubakyun4869@gmail.com>
2023-06-15 23:51:37 +0900

    [add]アイテムを点滅化

--- Assets/MyScript/FlashObject.cs
+++ Assets/MyScript/FlashObject.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class FlashObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    SpriteRenderer _spriteRenderer;
+    float _a_color;
+    [Tooltip("ìßñæÇ©")] bool isTransparent;
+
     void Start()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // èâä˙ÇÕïsìßñæ 
+        _a_color = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // ìßñæÇ…Ç»Ç¡ÇΩÇÁ 
+        if(_a_color <= 0)
+        {
+            isTransparent = true;
+        }
+        // ïsìßñæÇ…Ç»Ç¡ÇΩÇÁ 
+        else if(_a_color >= 1)
+        {
+            isTransparent = false;
+        }
         
+        // ïsìßñæÇ…Ç∑ÇÈ 
+        if(isTransparent)
+        {
+            _a_color += Time.deltaTime * 1.5f;
+        }
+        // ìßñæÇ…Ç∑ÇÈ 
+        else if (! isTransparent)
+        {
+            _a_color -= Time.deltaTime * 1.5f;
+        }
+
+        _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, _a_color);
     }
 }
